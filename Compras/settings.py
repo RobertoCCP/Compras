aaa38ac44f4ade75bf8f7e8bcd2748922f6e1ae6
RobertoCCP <rccuascotap@utn.edu.ec>
2023-12-25 23:12:37 -0500

    S

--- Compras/settings.py
+++ Compras/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_URL = 'static/'
 STATIC_URL = '/static/'
 
 # Following settings only make sense on production and may break development environments.
