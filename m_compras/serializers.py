387d9105e3fd23937cae00d6e080afdac90b2c85
RobertoCCP <rccuascotap@utn.edu.ec>
2023-12-25 20:48:48 -0500

    arreglo

---
+++ m_compras/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Providers
+
+class ProvidersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Providers
+        fields = '__all__'
