a16d2bf47fe12679ed203834b7f8e021a539c99a
RobertoCCP <rccuascotap@utn.edu.ec>
2023-12-26 00:01:30 -0500

    urls

--- Compras/urls.py
+++ Compras/urls.py
@@ -20,13 +20,27 @@ from m_compras.views import login_view
 from m_compras.views import consultar_pago
 from m_compras.views import ProvidersListCreateView
 from m_compras.views import consultar_proveedores
+from m_compras.views import login_view, menu_view, dashboard_view
+from m_compras.views import ProvidersListCreateView
+from m_compras.views import consultar_proveedores, editar_proveedor, insertar_proveedor
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('m_compras/', include('m_compras.urls')),
+     #path('m_compras/', include('m_compras.urls')),
     #path('', ProvidersListCreateView.as_view(), name='inicio'),  # Agrega esta línea para la ruta raíz
     #path('', consultar_pago, name='inicio'),
-    path('', login_view, name='inicio'),
     #path('', consultar_proveedores, name='inicio'),
+    path('', login_view, name='inicio'),
+     path('consultar_pago/', consultar_pago, name='consultar_pago'),
+    path('login/', login_view, name='login'),
+    path('menu/', menu_view, name='menu'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('api/tumodelo/', ProvidersListCreateView.as_view(), name='tumodelo-list-create'),
+    path('consultar_proveedores/', consultar_proveedores, name='consultar_proveedores'),
+    path('editar_proveedor/<int:prov_id>/', editar_proveedor, name='editar_proveedor'),
+    path('insertar_proveedor/', insertar_proveedor, name='insertar_proveedor'),
 ]
+
+
+
 
