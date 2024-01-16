137428d94f39f0959e715e640c1eb90782dead5b
RobertoCCP <rccuascotap@utn.edu.ec>
2024-01-16 17:47:02 -0500

    redespliegue

--- Compras/settings.py
+++ Compras/settings.py
@@ -77,27 +77,27 @@ WSGI_APPLICATION = 'Compras.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'Ac53EA24fBeA642dFa2gABF3CCDc6fD4',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '41100',
-    }
-}
-
 #DATABASES = {
  #   'default': {
   #      'ENGINE': 'django.db.backends.postgresql',
-   #     'NAME': 'pruebas',
-    #    'USER': 'usuario',
-     #   'PASSWORD': 'admin123',
-      #  'HOST': '45.70.13.16',
-       # 'PORT': '5432',
-   # }
+   #     'NAME': 'railway',
+    #    'USER': 'postgres',
+     #   'PASSWORD': 'Ac53EA24fBeA642dFa2gABF3CCDc6fD4',
+      #  'HOST': 'roundhouse.proxy.rlwy.net',
+      #  'PORT': '41100',
+    #}
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pruebas',
+        'USER': 'usuario',
+        'PASSWORD': 'admin123',
+        'HOST': '45.70.13.16',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
